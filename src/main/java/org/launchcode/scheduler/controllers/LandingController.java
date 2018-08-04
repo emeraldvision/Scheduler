351f86d1876ec87c05839118eb1dc1ae4d62627e
Ther Matthews <emeraldvision@gmail.com>
2018-08-04 18:31:50 -0500

    add test Thymeleaf templates

--- src/main/java/org/launchcode/scheduler/controllers/LandingController.java
+++ src/main/java/org/launchcode/scheduler/controllers/LandingController.java
@@ -1,22 +1,24 @@
 package org.launchcode.scheduler.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LandingController {
 
     @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "Scheduler App Homepage";
+    public String index(Model model) {
+
+        model.addAttribute("title", "Scheduler App | Home");
+        return "index";
     }
 
     @RequestMapping(value = "add")
-    @ResponseBody
-    public String addTask() {
-        return "Add Task Page";
+    public String addTask(Model model) {
+
+        model.addAttribute("title", "Add Task");
+        return "add-task";
     }
 
 }
